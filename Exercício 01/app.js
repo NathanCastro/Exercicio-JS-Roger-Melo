a68a172779e599ee1d0e216199365004ea8a9743
Nathan Castro <nathanslc18@gmail.com>
2022-08-02 07:39:36 -0300

    aula 1

---
+++ Exercício 01/app.js
@@ -0,0 +1,19 @@
+//alert("JS is Awesome!" )
+
+const myBirthYear = 1990;
+const currentYear = 2022;
+
+// console.log("const myBirthYear e currentYear", myBirthYear, currentYear);
+
+const myAge = (currentYear - myBirthYear)
+
+// console.log('minha idade é:', myAge);
+
+const ten = 10;
+
+const valor1 = currentYear + ten;
+const valor2 =  myAge + ten;
+
+console.log(`Em ${valor1}, estarei com ${valor2} anos.`)
+
+// Exercicio 1 finalizado
