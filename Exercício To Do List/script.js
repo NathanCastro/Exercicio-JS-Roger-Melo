5b973045ddb1671568a869d7f16194d29e527251
Nathan Castro <nathanslc18@gmail.com>
2022-11-13 22:40:27 -0300

    chore(todolist): first commit toDoList

---
+++ Exercício To Do List/script.js
@@ -0,0 +1,8 @@
+const formulario = document.querySelector('form')
+
+formulario.addEventListener('submit', event => {
+   event.preventDefault()
+    const inputValue = document.querySelector('.input__son').value
+
+    const newOi = document.createElement('p')
+})
