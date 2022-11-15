4d336ff244b85e5e250fbe544ace67449b29a571
Nathan Castro <nathanslc18@gmail.com>
2022-11-15 19:30:13 -0300

    chore(todolist): submit and click create

--- Exercício To Do List/script.js
+++ Exercício To Do List/script.js
@@ -1,8 +1,22 @@
 const formulario = document.querySelector('form')
+const inputValue = document.querySelector('.input__son');
+const elementDiv = document.querySelector('.boxList')
+const elementsInsideDiv = Array.from(elementDiv.children)
 
 formulario.addEventListener('submit', event => {
-   event.preventDefault()
-    const inputValue = document.querySelector('.input__son').value
-
-    const newOi = document.createElement('p')
-})
+    event.preventDefault()
+    const newParagraph = document.createElement('p');
+    newParagraph.textContent = inputValue.value;
+    newParagraph.classList.add('list');
+    elementDiv.appendChild(newParagraph);
+    inputValue.value = ""
+    inputValue.getAttribute('newParagraph')
+    
+    const novaListaParagrafo = document.querySelectorAll('p')
+    novaListaParagrafo.forEach( item => {
+        item.addEventListener('click', event => {
+            
+            console.log("🚀 ~ file: script.js ~ line 19 ~ event.target", event.target)
+        })
+    })
+});
