6af8c3b83331ab3ed8dc1e30d99b19abdc1107d2
Nathan Castro <nathanslc18@gmail.com>
2022-11-19 01:53:45 -0300

    chore(todolist): removed add

--- Exercício To Do List/script.js
+++ Exercício To Do List/script.js
@@ -5,6 +5,9 @@ const elementsInsideDiv = Array.from(elementDiv.children)
 
 formulario.addEventListener('submit', event => {
     event.preventDefault()
+    if(inputValue.value === ""){
+        alert('Digitar uma tarefa por favor')
+    }
     const newParagraph = document.createElement('p');
     newParagraph.textContent = inputValue.value;
     newParagraph.classList.add('list');
@@ -12,11 +15,15 @@ formulario.addEventListener('submit', event => {
     inputValue.value = ""
     inputValue.getAttribute('newParagraph')
     
+    
     const novaListaParagrafo = document.querySelectorAll('p')
     novaListaParagrafo.forEach( item => {
         item.addEventListener('click', event => {
             
-            console.log("🚀 ~ file: script.js ~ line 19 ~ event.target", event.target)
+            console.log("🚀 ~ file: script.js ~ line 18 ~ event", event)
+            const teste = event.target
+            console.log("🚀 ~ file: script.js ~ line 19 ~ teste", teste)
+            teste.remove()
         })
     })
-});
+}); 
