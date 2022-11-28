e0082a64b8d4ff5511aa006cf19291938a6c4728
Nathan Castro <nathanslc18@gmail.com>
2022-11-27 23:01:23 -0300

    chore(todolist): refactoring the code

--- Exercício To Do List/script.js
+++ Exercício To Do List/script.js
@@ -1,9 +1,15 @@
 const formulario = document.querySelector('form')
 const inputValue = document.querySelector('.input__son');
+const inputResult = document.querySelector('.input_grandSon');
 const elementDiv = document.querySelector('.boxList')
 const elementsInsideDiv = Array.from(elementDiv.children)
+const divInput = document.querySelector('.divInput');
 
-formulario.addEventListener('submit', event => {
+
+// console.log("🚀 ~ file: script.js ~ line 5 ~ elementsInsideDiv", elementsInsideDiv)
+
+/* formulario.addEventListener('submit', event => {
+
     event.preventDefault()
     if(inputValue.value === ""){
         alert('Digitar uma tarefa por favor')
@@ -15,15 +21,50 @@ formulario.addEventListener('submit', event => {
     inputValue.value = ""
     inputValue.getAttribute('newParagraph')
     
-    
     const novaListaParagrafo = document.querySelectorAll('p')
     novaListaParagrafo.forEach( item => {
         item.addEventListener('click', event => {
-            
-            console.log("🚀 ~ file: script.js ~ line 18 ~ event", event)
             const teste = event.target
-            console.log("🚀 ~ file: script.js ~ line 19 ~ teste", teste)
-            teste.remove()
+            teste.style.textDecoration ='line-through';
         })
     })
-}); 
+});  */
+
+
+formulario.addEventListener('submit', event => {
+    event.preventDefault();
+    const txtValue = document.createTextNode(inputValue.value)
+
+    if(inputValue.value === ""){
+        alert('Digite uma tarefa por favor')
+    }
+    let i;
+    // for (i = 0; i<inputValue.length; i++){
+    //     // inputResult.value = inputValue.value;
+    //     const maisInput = document.createElement("input")
+    //     console.log("🚀 ~ file: script.js ~ line 44 ~ maisInput", maisInput)
+    //     maisInput.classList.add = "input_grandSon"
+    //     maisInput.value = inputResult.value
+    //     inputValue[i].value.appendChild(maisInput)
+    // }
+
+    const maisInput = document.createElement("input")
+    maisInput.classList.add = "input_grandSon"
+    maisInput.appendChild(txtValue);
+    
+
+    inputValue.value = ""
+
+})
+
+
+
+
+
+/*
+1- Segundo input começar com display none;
+
+2- Ao colocar o valor no 1º input, aparecer esse valor no 2º input
+
+3- Quando clicar no checkin aparecer o risco no input[text-decoration:line-through]
+*/ 
